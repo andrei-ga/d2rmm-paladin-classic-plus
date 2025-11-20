4f8a36792ed3e7c30b0569221acdb1a196d8c650
andrei-ga <g.andrei.georgescu@gmail.com>
2025-11-20 19:34:52 +0200

    Fix Vengeance splash

--- src/mod.js
+++ src/mod.js
@@ -1,5 +1,5 @@
-const vengeanceSalvationDmgSynergy = +config.vengeanceSalvationDmgSynergy;
 const vengeanceSalvationSplashSynergy = +config.vengeanceSalvationSplashSynergy;
+const vengeanceResistSplashSynergy = +config.vengeanceResistSplashSynergy;
 const vengeanceSacrificeArSynergy = +config.vengeanceSacrificeArSynergy;
 const vengeanceExplosionRadius = +config.vengeanceExplosionRadius;
 
@@ -17,12 +17,6 @@ skills.rows.forEach((row) => {
         // original AR + % per Sacrifice hard point
         row.ToHitCalc = `${baseToHit} + (lvl-1)*${baseLevToHit} + skill('Sacrifice'.blvl)*${vengeanceSacrificeArSynergy}`;
 
-        // Make Salvation the only synergy for elemental % (simple version)
-        row.Param7 = vengeanceSalvationDmgSynergy;
-        row.calc1 = "ln12+skill('Salvation'.blvl)*par7";
-        row.calc2 = "ln12+skill('Salvation'.blvl)*par7";
-        row.calc3 = "ln12+skill('Salvation'.blvl)*par7";
-
         if (vengeanceExplosionRadius) {
             // Turn Vengeance into a "melee + missiles" skill
             row.auralencalc = 60;
@@ -30,14 +24,7 @@ skills.rows.forEach((row) => {
 
             // All missiles point to the custom AoE missile
             row.cltmissile  = "vengeancemissile";
-            row.cltmissilea = "vengeancemissile";
-            row.cltmissileb = "vengeancemissile";
-            row.cltmissilec = "vengeancemissile";
-
             row.srvmissile  = "vengeancemissile";
-            row.srvmissilea = "vengeancemissile";
-            row.srvmissileb = "vengeancemissile";
-            row.srvmissilec = "vengeancemissile";
         }
     }
 });
@@ -50,59 +37,12 @@ const skilldesc = D2RMM.readTsv(skilldescFilename);
 
 skilldesc.rows.forEach((row) => {
     if (row.skilldesc === "vengeance") {
-        // Adjust skill text to show correct calculation
-        row.desccalca2 = "ln12+skill('Salvation'.blvl)*par7";
-        row.desccalca4 = "ln12+skill('Salvation'.blvl)*par7";
-        row.desccalca5 = "ln12+skill('Salvation'.blvl)*par7";
-
-        // Remove Resist Passive Synergies
-        row.dsc3line3 = "";
-        row.dsc3texta3 = "";
-        row.dsc3textb3 = "";
-        row.dsc3calca3 = "";
-
-        row.dsc3line4 = "";
-        row.dsc3texta4 = "";
-        row.dsc3textb4 = "";
-        row.dsc3calca4 = "";
-
-        // Add Salvation first row
-        row.dsc3line2 = 76;
-        row.dsc3texta2 = "ElemDampLev";
-        row.dsc3textb2 = "skillname125";
-        row.dsc3calca2 = "par7";
-
-        // Remove previous Salvation row
-        row.dsc3line5 = "";
-        row.dsc3texta5 = "";
-        row.dsc3textb5 = "";
-        row.dsc3calca5 = "";
-
-        // Add missiles for calculation references
-        row.descmissile1 = "vengeancemissile_fire";
-        row.descmissile2 = "vengeancemissile_cold";
-        row.descmissile3 = "vengeancemissile_ltng";
-
-        // Add AoE dmg numbers in place of Cold Length
-        row.descline3 = 75;
-        row.desctexta3 = "VengeanceElemDmgAoE";
-        row.desctextb3 = "";
-        row.desccalca3 = "m1en";
-        row.desccalcb3 = "m1ex";
-
-        // Add Salvation Synergy text for AoE Damage
-        row.dsc3line3 = 76;
-        row.dsc3texta3 = "VengeanceElemDmgAoESynergy";
-        row.dsc3textb3 = "skillname125";
-        row.dsc3calca3 = vengeanceSalvationSplashSynergy;
-        row.dsc3calcb3 = "";
-
         // Add Sacrifice Attack Rating synergy text
-        row.dsc3line4  = 76;
-        row.dsc3texta4 = "VengeanceARSacrificeSynergy";
-        row.dsc3textb4 = "skillname96";
-        row.dsc3calca4 = vengeanceSacrificeArSynergy;
-        row.dsc3calcb4 = "";
+        row.dsc3line6  = 76;
+        row.dsc3texta6 = "AttRateplev";
+        row.dsc3textb6 = "skillname96";
+        row.dsc3calca6 = vengeanceSacrificeArSynergy;
+        row.dsc3calcb6 = "";
 
         if (vengeanceExplosionRadius) {
             row.dsc2line1 = 36;
@@ -110,6 +50,16 @@ skilldesc.rows.forEach((row) => {
             row.dsc2textb1 = '';
             row.dsc2calca1 = `${vengeanceExplosionRadius}`;
             row.dsc2calcb1 = '';
+
+            row.descmissile1 = "vengeancemissile_fire";
+            row.descmissile2 = "vengeancemissile_cold";
+            row.descmissile3 = "vengeancemissile_ltng";
+
+            row.descline3 = 75;
+            row.desctexta3 = "VengeanceElemDmgAoE";
+            row.desctextb3 = "";
+            row.desccalca3 = "m1en + m2en + m3en";
+            row.desccalcb3 = "m1ex + m2ex + m3ex";
         }
     }
 });
@@ -140,7 +90,6 @@ missiles.rows.push({
     AlwaysExplode: 1,
     GetHit: 1,
     Trans: 1,
-    MissileSkill: 1,
     ResultFlags: 5,
     HitFlags: 2,
     HitShift: 8,
@@ -170,14 +119,12 @@ for (let elem of ["cold", "fire", "ltng"]) {
         AlwaysExplode: 1,
         GetHit: 1,
         Trans: 1,
-        MissileSkill: 1,
         ResultFlags: 5,
         HitFlags: 2,
         HitShift: 8,
         NumDirections: 1,
-        EType: elem,
 
-        // Base ele damage and level scaling
+        EType: elem,
         EMin: 4,
         MinELev1: 3,
         MinELev2: 4,
@@ -190,13 +137,15 @@ for (let elem of ["cold", "fire", "ltng"]) {
         MaxELev2: 4,
         MaxELev3: 5,
         MaxELev4: 6,
-        MaxELev5: 9,
+        MaxELev5: 8,
 
         // Cold gets chill length; others don't
         ELen: (elem === "cold" ? 75 : ""),
 
-        // Synergy: Salvation increases splash damage
-        EDmgSymPerCalc: `(skill('Salvation'.blvl))*${vengeanceSalvationSplashSynergy}`,
+        // Synergies with Resist Cold/Fire/Lightning
+        EDmgSymPerCalc: elem === 'cold' ? `(skill('Resist Cold'.blvl))*${vengeanceResistSplashSynergy} + (skill('Salvation'.blvl))*${vengeanceSalvationSplashSynergy}` : elem === 'fire'
+            ? `(skill('Resist Fire'.blvl))*${vengeanceResistSplashSynergy} + (skill('Salvation'.blvl))*${vengeanceSalvationSplashSynergy}`
+            : `(skill('Resist Lightning'.blvl))*${vengeanceResistSplashSynergy} + (skill('Salvation'.blvl))*${vengeanceSalvationSplashSynergy}`,
         "*eol": 0
     });
 }
@@ -206,59 +155,30 @@ D2RMM.writeTsv(missilesFilename, missiles);
 // ===== skills.json =====
 const skillsJsonFileName = 'local\\lng\\strings\\skills.json';
 const skillsJson = D2RMM.readJson(skillsJsonFileName);
-let skillId = Math.max(...skillsJson.map((row) => row['id']));
+let skillIds = skillsJson.map((row) => row.id);
+let skillId = 92040;
+while (skillIds.includes(skillId)) {
+    skillId++;
+}
+console.log(`Adding skill ${skillId} to skills.json`);
 
 skillsJson.push(
     {
-        "id": ++skillId,
+        "id": skillId,
         "Key": "VengeanceElemDmgAoE",
-        "enUS": "Fire/Light/Cold Splash Damage: %d-%d",
-        "zhTW": "火焰/閃電/冰冷範圍傷害：%d-%d",
-        "deDE": "Feuer-/Blitz-/Kälte-Flächenschaden: %d-%d",
-        "esES": "Daño de salpicadura de Fuego/Rayo/Frío: %d-%d",
-        "frFR": "Dégâts de zone Feu/Foudre/Froid : %d-%d",
-        "itIT": "Danni ad area Fuoco/Fulmine/Freddo: %d-%d",
-        "koKR": "화염/번개/냉기 범위 피해: %d-%d",
-        "plPL": "Obrażenia obszarowe od Ognia/Błyskawic/Zimna: %d-%d",
-        "esMX": "Daño de área de Fuego/Rayo/Frío: %d-%d",
-        "jaJP": "火炎/稲妻/冷気の範囲ダメージ: %d-%d",
-        "ptBR": "Dano em área de Fogo/Raio/Frio: %d-%d",
-        "ruRU": "Урон по области огнём/молнией/холодом: %d–%d",
-        "zhCN": "火焰/闪电/冰冷范围伤害：%d-%d"
-    },
-    {
-        "id": ++skillId,
-        "Key": "VengeanceElemDmgAoESynergy",
-        "enUS": "%s: %+d%% Fire/Light/Cold Splash Damage per Level",
-        "zhTW": "%s：每級火焰/閃電/冰冷範圍傷害 %+d%%",
-        "deDE": "%s: %+d%% Feuer-/Blitz-/Kälte-Flächenschaden pro Stufe",
-        "esES": "%s: %+d%% de daño de salpicadura de Fuego/Rayo/Frío por nivel",
-        "frFR": "%s : %+d%% de dégâts de zone Feu/Foudre/Froid par niveau",
-        "itIT": "%s: %+d%% danni ad area Fuoco/Fulmine/Freddo per livello",
-        "koKR": "%s: 레벨당 화염/번개/냉기 범위 피해 %+d%%",
-        "plPL": "%s: %+d%% obrażeń obszarowych od Ognia/Błyskawic/Zimna za poziom",
-        "esMX": "%s: %+d%% de daño de área de Fuego/Rayo/Frío por nivel",
-        "jaJP": "%s: レベルにつき火炎/稲妻/冷気の範囲ダメージ %+d%%",
-        "ptBR": "%s: %+d%% de dano em área de Fogo/Raio/Frio por nível",
-        "ruRU": "%s: %+d%% урона по области огнём/молнией/холодом за уровень",
-        "zhCN": "%s：每级火焰/闪电/冰冷范围伤害 %+d%%"
-    },
-    {
-        "id": ++skillId,
-        "Key": "VengeanceARSacrificeSynergy",
-        "enUS": "%s: +%d%% Attack Rating per Level",
-        "zhTW": "%s：每級 +%d%% 攻擊命中率",
-        "deDE": "%s: +%d%% Angriffswert pro Stufe",
-        "esES": "%s: +%d%% al índice de ataque por nivel",
-        "frFR": "%s : +%d%% à l'indice d'attaque par niveau",
-        "itIT": "%s: +%d%% all'indice d'attacco per livello",
-        "koKR": "%s: 레벨당 명중률 +%d%%",
-        "plPL": "%s: +%d%% do współczynnika trafienia za poziom",
-        "esMX": "%s: +%d%% a la puntería de ataque por nivel",
-        "jaJP": "%s: レベルにつき攻撃命中率 +%d%%",
-        "ptBR": "%s: +%d%% na Taxa de Ataque por nível",
-        "ruRU": "%s: +%d%% к шансу попадания за уровень",
-        "zhCN": "%s：每级 +%d%% 攻击命中率"
+        "enUS": "Elemental Explosion Damage: %d-%d",
+        "zhTW": "元素爆炸傷害: %d-%d",
+        "deDE": "Elementarer Explosionsschaden: %d-%d",
+        "esES": "Daño de explosión elemental: %d-%d",
+        "frFR": "Dégâts d'explosion élémentaire : %d-%d",
+        "itIT": "Danni da esplosione elementale: %d-%d",
+        "koKR": "원소 폭발 피해: %d-%d",
+        "plPL": "Obrażenia od wybuchu żywiołów: %d-%d",
+        "esMX": "Daño de explosión elemental: %d-%d",
+        "jaJP": "元素爆発ダメージ: %d-%d",
+        "ptBR": "Dano de Explosão Elemental: %d-%d",
+        "ruRU": "Урон от стихийного взрыва: %d-%d",
+        "zhCN": "元素爆炸伤害: %d-%d"
     }
 );
 
